ee9642f48af1be3653da36fb8d3e5ae59b736e9f
Rianer <aerhan2001@gmail.com>
2022-10-31 18:12:21 +0200

    Lab4 final

--- Lab 4/queens.py
+++ Lab 4/queens.py
@@ -102,6 +102,20 @@ class Board:
             #self.process_queen(coordinate)
             return True
         return False
+    
+    def place_queen_copy(self, coordinate, board):
+        if board[coordinate[0]][coordinate[1]] == 0:
+            board[coordinate[0]][coordinate[1]] = 1
+            #self.process_queen(coordinate)
+            return True
+        return False
+    
+    def process_queen_copy(self, coordinate, board):
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if(i == coordinate[0] or j == coordinate[1] or i-coordinate[0] == j - coordinate[1] or i-coordinate[0] == coordinate[1] -j):
+                    if(board[i][j] == 0):
+                        board[i][j] = -2
 
     # OBSOLETE - checked where could the queen at the provided coordinate attack on the table
     def process_queen(self, coordinate):
@@ -110,6 +124,15 @@ class Board:
                 if(i == coordinate[0] or j == coordinate[1] or i-coordinate[0] == j - coordinate[1] or i-coordinate[0] == coordinate[1] -j):
                     if(self.board[i][j] == 0):
                         self.board[i][j] = -2
+
+    def clear_queen(self, row, col, board):
+        print(row)
+        #self.board[row][col] = 0
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if(i == row or j == col or i-row == j - col or i-row == col -j):
+                    if(board[i][j] == 0):
+                        board[i][j] = 0
 
     #Prints all the columns (domains) - not used in the algorithm
     def show_domains(self):
@@ -208,8 +231,103 @@ class Board:
         print(next_row)
         return next_row
 
+    def get_free_rows(self, col, board):
+        rows = []
+        index = 0
+        for row in board:
+            if row[col] == 0:
+                rows.append(index)
+            index += 1
+        return rows
+
+    def sort_cols(self, columns):
+        scores = dict()
+        for col in columns:
+            score = 0
+            for row in range(0, self.size):
+                if self.board[row][col] == 0:
+                    score += 1
+            scores[col] = score
+        scores = dict(sorted(scores.items(), key=lambda item: item[1]))
+        return list(scores.keys())
+
+    def get_free_cols(self, board):
+        cols = []
+        for col in range(self.size):
+            col_is_free = True
+            for row in range(self.size):
+                if board[row][col] == 1:
+                    col_is_free = False
+            if col_is_free:
+                cols.append(col)
+        return cols
+
+    def update_table(self):
+        board_copy = list(self.board)
+        for i in range(self.size):
+            for j in range(self.size):
+                if self.board[i][j] == 1:
+                    self.place_queen_copy([i,j],board_copy)
+                    self.process_queen_copy([i,j], board_copy)
+        return board_copy
+
+
+    def get_MRV(self):
+        board_copy = self.update_table()
+        cols = self.get_free_cols(board_copy)
+        cols = self.sort_cols(cols)
+        free_rows = self.get_free_rows(cols[0], board_copy)
+        result = []
+        for row in free_rows:
+            result.append([cols[0], row])
+        return result
+
+    def solve_NQueen_MRV(self, queen): 
+        # queen represents the queen number or the column (ex: queen = 2 => it is the third queen to be placed
+        # or in other words, the column that is being researched is the column 2,
+        # as we already placed a queen per column on the previous ones)
+
+        # if the current queen is the n-th queen, where n = size of the board, we should stop
+        if queen == self.size:
+            self.show()
+            return True
+
+        potentialRows = self.get_potential_rows(queen)
+
+        for row in potentialRows:
+            # trying to place a queen on every available row
+            self.place_queen([row, queen])
+
+            # considering that the domain is still available
+            domain_exhausted = False 
+
+            # looping through all still available potential future queen positions
+            for element in self.get_MRV():
+                if self.forward_check(element[0], element[1]):
+                    # The domain is exhausted if we place the queen at the current coordinates: [row, queen]
+                    domain_exhausted = True 
+                    break
+            
+            # The domain is still available, so we can continue
+            if not domain_exhausted:
+                # Checking for the next column (queen)
+                if self.solve_NQueen_FC(queen+1):
+                    return True
+
+             # Resetting the choice in case of a failure
+            self.board[row][queen] = 0
+
+
+    
+
+
 if __name__ == '__main__':
     #b1 = Board(4, [[1, 3], [1, 2], [2, 2]])
-    b1 = Board('block-10-58-1.param')
+    # 'block-10-58-1.param'
+    # 'block-10-50-11.param'
+    # 'block-10-50-14.param'
+    #
+    #
+    b1 = Board('block-10-50-11.param')
     #b1.show()
-    b1.solve_NQueen_FC(0)
+    b1.solve_NQueen_MRV(0)
